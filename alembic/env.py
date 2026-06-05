0106c7f567180d498f66bfbd26858f31fa4dd282
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-04 23:01:20 -0400

    alembic configuration for sp creation

--- alembic/env.py
+++ alembic/env.py
@@ -1,30 +1,23 @@
+import os
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+from dotenv import load_dotenv
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+from app.model.location import PetLocation
+from advanced_alchemy.base import UUIDBase
+
+load_dotenv()
+
 config = context.config
+db_url = os.getenv("DATABASE_URL","").replace("+asyncpg", "")
+config.set_main_option("sqlalchemy.url", db_url)
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
+target_metadata = UUIDBase.metadata
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -50,6 +43,11 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
+def ignore_postgis_tables(object, name, type_, reflected, compare_to):
+    if type_ == "table" and name == "spatial_ref_sys":
+        return False
+    return True
+
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
@@ -65,7 +63,9 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            include_object=ignore_postgis_tables
         )
 
         with context.begin_transaction():
