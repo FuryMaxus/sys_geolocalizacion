46b3173946da6925def233d9f0db93d08013e7e2
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-04 21:59:37 -0400

    database config creation, added libraries to requirements, exmple env, git ignore, and basic litestar function

---
+++ app/core/db_config.py
@@ -0,0 +1,12 @@
+import os
+from advanced_alchemy.extensions.litestar import SQLAlchemyPlugin, SQLAlchemyAsyncConfig
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
+
+db_config = SQLAlchemyAsyncConfig(
+    connection_string= DATABASE_URL
+)
+
+db_plugin = SQLAlchemyPlugin(config=db_config)
