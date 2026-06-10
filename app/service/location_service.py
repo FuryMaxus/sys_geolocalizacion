e26ecb17f6db0aa0dfbdef0f36b821d08b46d598
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-10 16:32:40 -0400

    changed type in coordinate, updated location service

--- app/service/location_service.py
+++ app/service/location_service.py
@@ -1,37 +1,42 @@
 import uuid
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import cast
 from app.repository.location_repository import LocationRepository
 from app.model.location import PetLocation
+from geoalchemy2.elements import WKBElement, WKTElement
 
 class LocationService:
     def __init__(self, db_session: AsyncSession):
         self.repository = LocationRepository(session=db_session)
 
     async def upsert_location(
-            self,
-            pet_id: uuid.UUID,
-            latitude: float, 
-            longitude: float
-        ) -> PetLocation:
-        point_wkt = f"POINT({longitude} {latitude})"
+        self,
+        pet_id: uuid.UUID,
+        latitude: float, 
+        longitude: float
+    ) -> PetLocation:
+        point_wkt = WKTElement(f"POINT({longitude} {latitude})", srid=4326)
         existing_location = await self.repository.get_one_or_none(pet_id=pet_id)
         if existing_location:
-            existing_location.coordinate = point_wkt
+            existing_location.coordinate = cast(WKBElement, point_wkt)
             return await self.repository.update(existing_location)
         else:
-            new_location = PetLocation(pet_id=pet_id, coordinate=point_wkt)
+            new_location = PetLocation(
+                pet_id=pet_id, 
+                coordinate=cast(WKBElement, point_wkt)
+            )
             return await self.repository.add(new_location)
 
     async def get_location(
-            self,
-            pet_id: uuid.UUID
-        ) -> PetLocation | None:
+        self,
+        pet_id: uuid.UUID
+    ) -> PetLocation | None:
         return await self.repository.get_one_or_none(pet_id=pet_id)
 
     async def delete_location(
-            self,
-            pet_id: uuid.UUID
-        ) -> None:
+        self,
+        pet_id: uuid.UUID
+    ) -> None:
         existing_location = await self.repository.get_one_or_none(pet_id=pet_id)
         if existing_location:
             await self.repository.delete(existing_location.id)
