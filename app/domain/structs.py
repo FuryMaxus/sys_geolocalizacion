c26a53ee9862f488ea43445b7a109080ac874845
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-10 15:44:24 -0400

    added structs for crud

---
+++ app/domain/structs.py
@@ -0,0 +1,34 @@
+import msgspec
+from uuid import UUID
+class DistanceRequest(msgspec.Struct):
+    origin_latitude: float
+    origin_longitude: float
+    target_latitude: float
+    target_longitude: float
+
+    def __post_init__(self):
+        if not (-90.0 <= self.origin_latitude <= 90.0) or not (-90.0 <= self.target_latitude <= 90.0):
+            raise ValueError("Latitude must be between -90 and 90")
+        if not (-180.0 <= self.origin_longitude <= 180.0) or not (-180.0 <= self.target_longitude <= 180.0):
+            raise ValueError("Longitude must be between -180 and 180")
+        
+class DistanceResponse(msgspec.Struct):
+    distance_meters: float
+
+class LocationCreateRequest(msgspec.Struct):
+    pet_id: UUID
+    latitude: float
+    longitude: float
+
+    def __post_init__(self):
+        if not (-90.0 <= self.latitude <= 90.0):
+            raise ValueError("Latitude must be between -90 and 90")
+        if not (-180.0 <= self.longitude <= 180.0):
+            raise ValueError("Longitude must be between -180 and 180")
+        
+class LocationResponse(msgspec.Struct):
+    pet_id: UUID
+    coordinate: str  # temporal
+
+class MessageResponse(msgspec.Struct):
+    message: str
